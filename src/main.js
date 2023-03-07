a8356a2f55485268bdd56484aa801a771a91e6cc
吴振宇 <wuzhenyu@troila.com>
2023-03-07 11:49:48 +0800

    支持购物车数量和总价显示

--- src/main.js
+++ src/main.js
@@ -1,13 +1,28 @@
 import "./app.css";
 import App from "./App.svelte";
+import Big from "big.js";
 
 window.WohemamaCartSettings = {
   publicApiKey: "wuzhenyu@mail.smallsticker.com",
   shipping: 13,
 };
 
+const parsedCart = JSON.parse(localStorage.cartData ?? null) || [];
+
 const target = document.querySelector("body");
 let widget = undefined;
+
+const itemCountTarget = document.querySelector(".wohemama-items-count");
+if (itemCountTarget)
+  itemCountTarget.textContent = parsedCart
+    .map((i) => i.itemCount)
+    .reduce((m, n) => new Big(m).plus(n), 0);
+
+const totalPriceTarget = document.querySelector(".wohemama-total-price");
+if (totalPriceTarget)
+  totalPriceTarget.textContent = parsedCart
+    .map((i) => new Big(i.itemPrice).times(i.itemCount))
+    .reduce((m, n) => new Big(m).plus(n), 0);
 
 document.addEventListener("click", (e) => {
   if (
@@ -33,9 +48,7 @@ document.addEventListener("click", (e) => {
   ) {
     if (widget) widget.$destroy();
     const detail = e.target.dataset;
-    console.log(detail, detail.itemUrl === undefined);
     if (detail.itemUrl === undefined) detail.itemUrl = location.href;
-    console.log("detail2", detail, location.href);
     widget = new App({
       target,
       props: {
